017f86460ccb762a255cf269ff4dece4eacf8e35
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-01-20 14:58:32 +0100

    create Wercker project

--- test/config-test.js
+++ test/config-test.js
@@ -2,7 +2,9 @@ var Git = require('../lib/git.js').Git;
 
 exports.testGitConfigAvailable = function(test) {
     var git = new Git();
-    var result = git.readConfig();
-    test.ok(result, 'config should exist');
-    test.done();
+    git.readConfig(function(result){
+        test.notEqual(result, null, 'Config should not be null');
+        test.done();
+    });
 }
+
