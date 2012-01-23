16b55f63bf7bed7c6cfd33f3d676cf2b673b94e2
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-01-23 17:30:05 +0100

    test

--- test/config-test.js
+++ test/config-test.js
@@ -4,7 +4,7 @@ exports.testGitConfigAvailable = function(test) {
     var repo = new Repository();
     repo.readConfig(function(result){
         test.notEqual(result, null, 'Config should not be null');
-        test.done();
     });
+    test.done();
 };
 
