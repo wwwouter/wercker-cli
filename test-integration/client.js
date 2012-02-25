b5cf6553a3148ea0c33d66de13be2b06131dd7c0
wouterm <hebrox@gmail.com>
2012-02-25 20:48:16 +0100

    oauth

---
+++ test-integration/client.js
@@ -0,0 +1,32 @@
+
+var Client = require('../lib/client.js').Client;
+
+exports.test1= function(test) {
+    test.expect(0);
+
+
+
+    var client = new Client();
+
+  client.callPriv();
+
+  client.werckerUrl = 'http://localhost:3000';
+
+
+  client.getOAuthToken('1','1', 'cli', function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log('done')
+    }
+
+    test.done();
+  })
+
+
+
+
+};
+
+
